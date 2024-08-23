29ad3f19ebf22984f762b03ba53249357eb32491
SeunSarumi <seunsarumi@gmail.com>
2024-08-23 18:37:29 +0100

    react hook form installation and register function

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,11 +2,12 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  // useEffect(function () {
-  //   getCabins().then((data) => console.log(data));
-  // }, []);
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -16,6 +17,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
