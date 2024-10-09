c772b8fa2ea575df7eb4d1fd46f4620951e6e8ea
SeunSarumi <seunsarumi@gmail.com>
2024-10-09 01:51:36 +0100

    client side filtering for cabins table

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,13 +4,14 @@ import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />
