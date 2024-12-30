749a7cb127dba2c364b8dc3ac94ae1aff1636820
SeunSarumi <seunsarumi@gmail.com>
2024-12-30 23:42:32 +0000

    removed navigation from checkout function

--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -1,11 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateBooking } from "../../services/apiBookings";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
 
 export function useCheckOut() {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
   const { mutate: checkout, isLoading: isCheckingOut } = useMutation({
     mutationFn: (bookingId) =>
@@ -16,7 +14,6 @@ export function useCheckOut() {
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} successfully checked out`);
       queryClient.invalidateQueries({ active: true });
-      navigate("/");
     },
 
     onError: () => toast.error("There was an error while checking out "),
