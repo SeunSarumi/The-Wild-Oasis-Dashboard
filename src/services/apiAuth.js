3f30f06f1e396b04500c89747c443bb7160cbc00
SeunSarumi <seunsarumi@gmail.com>
2024-12-31 20:39:18 +0000

    initial auth setup

---
+++ src/services/apiAuth.js
@@ -0,0 +1,13 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  console.log(data);
+  return data;
+}
