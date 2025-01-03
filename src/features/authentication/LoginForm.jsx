9e72e5de706b02d3e5d84edcb37a989a2b70cf58
SeunSarumi <seunsarumi@gmail.com>
2025-01-03 19:42:33 +0000

    added clear field for wrong credientials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -14,7 +14,15 @@ function LoginForm() {
   function handleSubmit(e) {
     e.preventDefault();
     if (!email || !password) return;
-    login({ email, password });
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
   }
 
   return (
