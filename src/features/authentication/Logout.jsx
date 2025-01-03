05ab956dcbbfbf79979d05e8b54868cffa0d2b15
SeunSarumi <seunsarumi@gmail.com>
2025-01-03 20:50:32 +0000

    added logout btn to the header

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,12 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+
+function Logout() {
+  return (
+    <ButtonIcon>
+      <HiArrowRightOnRectangle />
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
