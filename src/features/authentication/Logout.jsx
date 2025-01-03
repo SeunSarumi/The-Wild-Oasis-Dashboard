f276c9f1109f194548aaf3976262b51d725f45c8
SeunSarumi <seunsarumi@gmail.com>
2025-01-03 21:15:43 +0000

    logout functionality built

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,10 +1,14 @@
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
 import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function Logout() {
+  const { logout, isLoading } = useLogout();
+
   return (
-    <ButtonIcon>
-      <HiArrowRightOnRectangle />
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {isLoading ? <SpinnerMini /> : <HiArrowRightOnRectangle />}
     </ButtonIcon>
   );
 }
