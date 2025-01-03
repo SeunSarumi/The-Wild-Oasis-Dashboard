94669b82022f197f47e495721567eb69a810907e
SeunSarumi <seunsarumi@gmail.com>
2025-01-03 19:40:24 +0000

    protect route functionality built

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,13 +1,42 @@
-function ProtectedRoute({ children }) {
-  // 1 Load the autenticated user
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-  // 2. while loading show a spinner
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  // 1 Load the autenticated user
+  const { isLoading, isAuthenticated } = useUser();
 
   // 3. if there is no autheticated user, redirect to login page
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 2. while loading show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />;
+      </FullPage>
+    );
 
   // 4, if there is user render the app
 
-  return children;
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
