3024ab57f82902602c97d1cd90ef56a6234e1fcb
SeunSarumi <seunsarumi@gmail.com>
2025-01-03 19:10:49 +0000

    ProtectedRoute wrapping and fucntion for session

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+function ProtectedRoute({ children }) {
+  // 1 Load the autenticated user
+
+  // 2. while loading show a spinner
+
+  // 3. if there is no autheticated user, redirect to login page
+
+  // 4, if there is user render the app
+
+  return children;
+}
+
+export default ProtectedRoute;
