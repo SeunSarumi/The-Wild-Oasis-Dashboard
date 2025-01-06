abe5398b71ecf5cb112df1d8af75c05e2ca223b5
SeunSarumi <seunsarumi@gmail.com>
2025-01-06 16:44:10 +0000

    header menu implemented v1

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+
+import HeaderMenu from "./HeaderMenu";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -10,7 +11,7 @@ const StyledHeader = styled.header`
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
