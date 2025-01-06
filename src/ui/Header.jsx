b901ac80f28099606deef37d941d0bfaa64b8eaa
SeunSarumi <seunsarumi@gmail.com>
2025-01-06 16:55:00 +0000

    default user avatar

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -11,6 +12,7 @@ const StyledHeader = styled.header`
 function Header() {
   return (
     <StyledHeader>
+      <UserAvatar />
       <HeaderMenu />
     </StyledHeader>
   );
