4e5776feca3647a7969f951f0908d515d30612f4
SeunSarumi <seunsarumi@gmail.com>
2025-01-06 16:56:33 +0000

    styling for app header for menu items

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -7,6 +7,11 @@ const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
