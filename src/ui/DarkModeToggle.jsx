720b721e35188a1407c616999630bff2019efa58
SeunSarumi <seunsarumi@gmail.com>
2025-01-07 21:35:06 +0000

    dark mode btn added to menu

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,12 @@
+import { HiOutlineMoon } from "react-icons/hi2";
+import ButtonIcon from "./ButtonIcon";
+
+function DarkModeToggle() {
+  return (
+    <ButtonIcon>
+      <HiOutlineMoon />
+    </ButtonIcon>
+  );
+}
+
+export default DarkModeToggle;
