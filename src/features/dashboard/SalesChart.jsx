8bc14d6832fc8f86c584482d2845e969ecdd27a3
SeunSarumi <seunsarumi@gmail.com>
2025-01-08 21:28:10 +0000

    changed the header of the bar chart

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -93,7 +93,10 @@ function SalesChart({ bookings, numDays }) {
 
   return (
     <StyledSalesChart>
-      <Heading as="h2">Sales</Heading>
+      <Heading as="h2">
+        Sales from {format(allDates.at(0), "MMM dd")} &mdash;{" "}
+        {format(allDates.at(-1), "MMM dd")}
+      </Heading>
 
       <ResponsiveContainer height={300} width="100%">
         <AreaChart data={data}>
