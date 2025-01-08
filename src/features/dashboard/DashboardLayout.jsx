dae3ec7fed76dd6cb2e3dcaba26b4865503bb29e
SeunSarumi <seunsarumi@gmail.com>
2025-01-08 20:42:58 +0000

    first chart without bookings

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -32,7 +33,7 @@ function DashboardLayout() {
       />
       <div>Today&apos;s activity</div>
       <div>Chart of stay duration</div>
-      <div>Chart of sales</div>
+      <SalesChart />
     </StyledDashboardLayout>
   );
 }
