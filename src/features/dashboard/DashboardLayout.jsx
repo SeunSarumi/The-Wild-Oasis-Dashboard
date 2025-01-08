b0fe695affe89031e59f726092f57357e400b757
SeunSarumi <seunsarumi@gmail.com>
2025-01-08 17:56:09 +0000

    initial dashboard setup

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,15 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today&apos;s activity</div>
+      <div>Chart of stay duration</div>
+      <div>Chart of sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
