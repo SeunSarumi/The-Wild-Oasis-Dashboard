ecf17db24c88846456c347d484eb58a322bcf2f7
SeunSarumi <seunsarumi@gmail.com>
2025-01-14 16:01:31 +0000

    trying to fix checkin error

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/Booking";
-import Checkin from "./pages/CheckIn";
+// import Checkin from "./pages/CheckIn";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import { DarkModeProvider } from "./context/DarkModeContext";
+import Checkin from "./pages/Checkin - Copy";
 
 const queryClient = new QueryClient({
   defaultOptions: {
